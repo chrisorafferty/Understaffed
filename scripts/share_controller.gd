95e3651b4425323ae1d3516231c7730bc19aadac
Chris O'Rafferty <christopher.orafferty@gmail.com>
2025-02-17 21:38:08 +1000

    Update graph to fit any screen size

--- scripts/share_controller.gd
+++ scripts/share_controller.gd
@@ -3,36 +3,41 @@ extends Control
 @export var line: Line2D
 
 var SHARE_UPDATE_TIME: float = 3.0
-var DIST_BETWEEN_GRAPH_POINTS: float = 20.0
+var DIST_BETWEEN_GRAPH_POINTS: float = 0.05
 var timer: float = 0
 var totalTime: float = 0
-var graphWidth: float = 0
-var graphHeight: float = 0
-var graphPoints: Array = []
+var graphPoints: Array[GraphPoint] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	line.position.x = 0.0
-	line.position.y = size.y
-	graphWidth = size.x
-	graphHeight = size.y
 	line.clear_points()
 	
-	line.add_point(Vector2(graphWidth, 0.3 * -graphHeight))
-	line.add_point(Vector2(graphWidth, 0.5 * -graphHeight))
-	graphPoints.append(totalTime - SHARE_UPDATE_TIME)
-	graphPoints.append(totalTime)
+	line.add_point(Vector2(size.x, 0.3 * -size.y))
+	line.add_point(Vector2(size.x, 0.5 * -size.y))
+	graphPoints.append(GraphPoint.new(totalTime - SHARE_UPDATE_TIME, 0.3))
+	graphPoints.append(GraphPoint.new(totalTime, 0.5))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	timer += delta
 	totalTime += delta
+	
+	line.position.x = 0.0
+	line.position.y = size.y
 
 	if timer >= SHARE_UPDATE_TIME:
 		timer -= SHARE_UPDATE_TIME
 		var yPos = clamp(randfn(0.5, 0.3), 0, 1)
-		line.add_point(Vector2(graphWidth, yPos * -graphHeight))
-		graphPoints.append(totalTime)
+		line.add_point(Vector2(size.x, yPos * -size.y))
+		graphPoints.append(GraphPoint.new(totalTime, yPos))
 
 	for i in range(graphPoints.size()):
-		line.set_point_position(i, Vector2((graphWidth - ((totalTime - graphPoints[i]) * DIST_BETWEEN_GRAPH_POINTS)), line.points[i].y))
+		line.set_point_position(i, Vector2((size.x - ((totalTime - graphPoints[i].time) * DIST_BETWEEN_GRAPH_POINTS * size.x)),  graphPoints[i].y * -size.y))
+
+class GraphPoint:
+	var time: float
+	var y: float
+	
+	func _init(_time: float, _y: float):
+		time = _time
+		y = _y
